f1c2c6ae693ceee7add16b18603956db3052594c
Marcos_Romano <mri1001@alu.ubu.es>
2021-05-17 18:45:28 +0200

    Move Method in the switch instructions of the print methods de Network into Node

--- src/lanSimulation/Network.java
+++ src/lanSimulation/Network.java
@@ -329,28 +329,7 @@ public class Network {
 		assert isInitialized();
 		Node currentNode = firstNode;
 		do {
-			switch (currentNode.type_) {
-			case Node.NODE:
-				buf.append("Node ");
-				buf.append(currentNode.name_);
-				buf.append(" [Node]");
-				break;
-			case Node.WORKSTATION:
-				buf.append("Workstation ");
-				buf.append(currentNode.name_);
-				buf.append(" [Workstation]");
-				break;
-			case Node.PRINTER:
-				buf.append("Printer ");
-				buf.append(currentNode.name_);
-				buf.append(" [Printer]");
-				break;
-			default:
-				buf.append("(Unexpected)");
-				;
-				break;
-			}
-			;
+			currentNode.printOn(buf);
 			buf.append(" -> ");
 			currentNode = send(currentNode);
 		} while (isFirstNode(currentNode));
@@ -375,28 +354,7 @@ public class Network {
 		buf.append("\n\n<UL>");
 		do {
 			buf.append("\n\t<LI> ");
-			switch (currentNode.type_) {
-			case Node.NODE:
-				buf.append("Node ");
-				buf.append(currentNode.name_);
-				buf.append(" [Node]");
-				break;
-			case Node.WORKSTATION:
-				buf.append("Workstation ");
-				buf.append(currentNode.name_);
-				buf.append(" [Workstation]");
-				break;
-			case Node.PRINTER:
-				buf.append("Printer ");
-				buf.append(currentNode.name_);
-				buf.append(" [Printer]");
-				break;
-			default:
-				buf.append("(Unexpected)");
-				;
-				break;
-			}
-			;
+			currentNode.printOn(buf);
 			buf.append(" </LI>");
 			currentNode = send(currentNode);
 		} while (isFirstNode(currentNode));
@@ -416,28 +374,7 @@ public class Network {
 		buf.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n<network>");
 		do {
 			buf.append("\n\t");
-			switch (currentNode.type_) {
-			case Node.NODE:
-				buf.append("<node>");
-				buf.append(currentNode.name_);
-				buf.append("</node>");
-				break;
-			case Node.WORKSTATION:
-				buf.append("<workstation>");
-				buf.append(currentNode.name_);
-				buf.append("</workstation>");
-				break;
-			case Node.PRINTER:
-				buf.append("<printer>");
-				buf.append(currentNode.name_);
-				buf.append("</printer>");
-				break;
-			default:
-				buf.append("<unknown></unknown>");
-				;
-				break;
-			}
-			;
+			currentNode.printXMLOn(buf);
 			currentNode = send(currentNode);
 		} while (isFirstNode(currentNode));
 		buf.append("\n</network>");
